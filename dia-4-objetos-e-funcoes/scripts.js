4d104e125c3f2c6ba0216b85b1cd389943dd86ca
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-06-24 18:58:14 -0300

    exercicio 5

--- dia-4-objetos-e-funcoes/scripts.js
+++ dia-4-objetos-e-funcoes/scripts.js
@@ -1,7 +1,14 @@
 let info = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    //recorrente: "Sim",
+};
+let outro = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim",
 };
 //console.log("Bem-vinda, " + info.personagem);
 info["recorrente"] = "Sim";
@@ -9,6 +16,10 @@ info["recorrente"] = "Sim";
 /*for(let index in info){
     console.log(index);
 }*/
-for(let index in info){
+/*for(let index in info){
     console.log(info[index]);
+}
+*/
+for (let i in info) {
+    console.log(info[i] + ' e ' + outro[i]);
 }
