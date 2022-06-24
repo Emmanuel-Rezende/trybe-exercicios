c3aafc018f1f426d079b0e6c9d23a66ff274cab4
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-06-24 18:41:49 -0300

    exercicios 1/2/3/4 feitos

---
+++ dia-4-objetos-e-funcoes/scripts.js
@@ -0,0 +1,14 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+//console.log("Bem-vinda, " + info.personagem);
+info["recorrente"] = "Sim";
+
+/*for(let index in info){
+    console.log(index);
+}*/
+for(let index in info){
+    console.log(info[index]);
+}
